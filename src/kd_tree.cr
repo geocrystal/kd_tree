237e04b3b3d1e3ebbb447e22a6a28c4fbb72d8db
Anton Maminov <anton.linux@gmail.com>
2024-04-11 19:23:39 +0300

    fix nearest points

--- src/kd_tree.cr
+++ src/kd_tree.cr
@@ -14,91 +14,71 @@ module Kd
       end
     end
 
-    getter root
-
-    @root : Node(T)?
+    getter root : Node(T)?
     @k : Int32
 
-    def initialize(points : Array(Array(T)), depth = 0)
+    def initialize(points : Array(Array(T)))
       @k = points.first.size # assumes all points have the same dimension
-      @root = build(points, depth)
+      @root = build_tree(points, 0)
     end
 
-    def nearest(query : Array(T))
-      nearest(query, 1)
+    def nearest(target : Array(T), n : Int32 = 1) : Array(Array(T))
+      return [] of Array(T) if n < 1
+
+      best_nodes = Array(Node(T)).new
+
+      find_n_nearest(@root, target, 0, best_nodes, n)
+
+      best_nodes.map(&.pivot)
     end
 
-    def nearest(query : Array(T), n : Int32)
-      nearest!(@root, query, [] of Array(T), n)
-    end
-
-    private def nearest!(
-      curr : Node?,
-      query : Array(T),
-      nearest : Array(Array(T)),
-      n = 1
+    private def find_n_nearest(
+      node : Node(T)?,
+      target : Array(T),
+      depth : Int32,
+      best_nodes : Array(Node(T)),
+      n : Int32
     )
-      return nearest if curr.nil?
+      return unless node
 
-      # if the current node is better than any of the current nearest,
-      # then it becomes a current nearest
-      if nearest.size < n
-        nearest << curr.pivot
-      else
-        dist_curr_query = distance(curr.pivot, query)
-        ix = nearest.index { |b| dist_curr_query < distance(b, query) }
-        nearest[ix] = curr.pivot if ix
+      axis = depth % @k
+
+      next_node = target[axis] < node.pivot[axis] ? node.left : node.right
+      other_node = target[axis] < node.pivot[axis] ? node.right : node.left
+
+      find_n_nearest(next_node, target, depth + 1, best_nodes, n)
+
+      if best_nodes.size < n || distance(target, node.pivot) < distance(target, best_nodes.last.pivot)
+        best_nodes << node
+        best_nodes.sort_by! { |nd| distance(target, nd.pivot) }
+        best_nodes.pop if best_nodes.size > n
       end
 
-      # determine which branch contains the query along the split dimension
-      nearer, farther = if query[curr.split] <= curr.pivot[curr.split]
-                          [curr.left, curr.right]
-                        else
-                          [curr.right, curr.left]
-                        end
-
-      # search the nearer branch
-      nearest = nearest!(nearer, query, nearest, n)
-
-      # search the farther branch if the distance to the hyperplane is less
-      # than any nearest so far
-      dist_curr_query_spldim = distance(
-        [curr.pivot[curr.split]],
-        [query[curr.split]]
-      )
-
-      if nearest.find { |b| distance(b, query) >= dist_curr_query_spldim }
-        nearest = nearest!(farther, query, nearest, n)
+      if other_node && (best_nodes.size < n || (target[axis] - node.pivot[axis]).abs**2 < distance(target, best_nodes.last.pivot))
+        find_n_nearest(other_node, target, depth + 1, best_nodes, n)
       end
-
-      # else no need to search the entire farther branch i.e. prune!
-      nearest
     end
 
     private def distance(m : Array(T), n : Array(T))
       # squared euclidean distance (to avoid expensive sqrt operation)
-      m.each_with_index.reduce(0) do |sum, (coord, index)|
-        sum += (coord - n[index]) ** 2
-        sum
+      m.each_with_index.sum do |coord, index|
+        (coord - n[index]) ** 2
       end
     end
 
-    private def build(points : Array(Array(T)), depth = 0)
+    private def build_tree(points : Array(Array(T)), depth : Int32) : Node(T)?
       return if points.empty?
 
-      # Select axis based on depth so that axis cycles through all valid values
       axis = depth % @k
-
-      # Sort point list and choose median as pivot element
-      points.sort! { |m, n| m[axis] <=> n[axis] }
-      pivot = points.size // 2
+      points.sort_by! { |point| point[axis] }
+      median = points.size // 2
 
       # Create node and construct subtrees
       Node(T).new(
-        points[pivot],
+        points[median],
         axis,
-        build(points[0...pivot], depth + 1),
-        build(points[pivot + 1..-1], depth + 1)
+        build_tree(points[0...median], depth + 1),
+        build_tree(points[median + 1..], depth + 1)
       )
     end
   end
