931657c7c7a65b0b9968d27be9fce2ee3a7d27b6
Anton Maminov <anton.linux@gmail.com>
2024-04-13 00:50:26 +0300

    Update benchmark.cr

--- benchmark/benchmark.cr
+++ benchmark/benchmark.cr
@@ -1,26 +1,25 @@
 require "benchmark"
 require "../src/kd_tree"
 
-# Generate 1 million random points
-points = Array.new(1_000_000) { [rand * 100.0, rand * 100.0] }
+# Generate 10 millions random points
+points = Array.new(10_000_000) { [rand * 100.0, rand * 100.0] }
 
-puts "Benchmarking KD-Tree with 1 million points"
-tree = nil
-build_time = Benchmark.measure {
-  tree = Kd::Tree(Array(Float64)).new(points)
-}
-
-puts "build(init): #{build_time.total.round(2)} seconds"
-
-tree = tree.not_nil!
-
-# Define a test point to find nearest neighbors for
-test_point = [50.0, 50.0]
+puts "Benchmarking KD-Tree with 10 millions points"
 
 Benchmark.bm do |x|
+  tree = nil
+
+  x.report("build(init)") {
+    tree = Kd::Tree(Array(Float64)).new(points)
+  }
+
   [1, 5, 10, 50, 100, 255, 999].each do |n|
     x.report("nearest point #{n.to_s.rjust(3, ' ')}") do
-      tree.not_nil!.nearest(test_point, n)
+      1000.times do
+        test_point = [rand * 100.0, rand * 100.0]
+
+        tree.not_nil!.nearest(test_point, n)
+      end
     end
   end
 end
